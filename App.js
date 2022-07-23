d03f6a474ab2d6d3f8dab01b25e0069e5926e7ad
joao nadal <jh.nadal@icloud.com>
2022-07-23 12:37:45 -0700

    Fixed the navigation bar, started the data structure

--- App.js
+++ App.js
@@ -1,14 +1,85 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { Settings, Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
+// const Tab = createBottomTabNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+function SettingsScreen() {
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+    </SafeArea>
+  );
+}
+
+function MapScreen() {
+  return (
+    <SafeArea>
+      <Text>Map</Text>
+    </SafeArea>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Restaurants"
+      activeColor="#FFF"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: "#FFF" }}
+      barStyle={{ backgroundColor: "tomato" }}
+    >
+      <Tab.Screen
+        name="Restaurants"
+        component={RestaurantsScreen}
+        options={{
+          tabBarLabel: "Restaurants",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="silverware" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          tabBarLabel: "Map",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="map-marker" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: "Settings",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   const [oswadLoaded] = useOswald({
@@ -24,12 +95,22 @@ export default function App() {
   }
 
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
-      </ThemeProvider>
-
-      <ExpoStatusBar style="auto" />
-    </>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </ThemeProvider>
+    // <>
+    //   <ThemeProvider theme={theme}>
+    //     <NavigationContainer>
+    //       <Tab.Navigator>
+    //         <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+    //         <Tab.Screen name="Map" component={Map} />
+    //         <Tab.Screen name="Settings" component={Settings} />
+    //       </Tab.Navigator>
+    //     </NavigationContainer>
+    //   </ThemeProvider>
+    //   <ExpoStatusBar style="auto" />
+    // </>
   );
 }
